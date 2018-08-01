c3238a8c351c60d0e99d5ba18b319bd815f4cf71
iwbinb <34509975+iwbinb@users.noreply.github.com>
2018-08-01 12:30:08 +0800

    Update mining.sh

--- mining.sh
+++ mining.sh
@@ -1,2 +1,2 @@
-echo "---StartARmining-----"
+echo "---Startmining-----"
 ./arweave-server peer 35.178.129.36 peer 18.130.126.49 peer 52.56.119.91 peer 52.33.231.154 peer 34.216.88.202 peer 54.159.24.25 peer 54.175.114.204 peer 13.231.142.3 peer 18.179.119.203 peer 54.93.54.173 peer 18.185.120.46 peer 138.197.131.159 peer 206.189.86.98 peer 209.97.175.74 peer 209.97.160.239 peer 209.97.160.159 peer 206.189.5.91 peer 167.99.99.34 peer 165.227.4.33 peer 206.189.170.147 peer 167.99.98.48 peer 204.48.25.210 peer 159.89.225.231 peer 204.48.27.27 peer 204.48.27.17 peer 209.97.142.167 peer 209.97.142.68 peer 209.97.134.129 peer 209.97.128.161 peer 206.189.121.5 peer 209.97.142.170 peer 209.97.142.169 peer 209.97.142.143 mine mining_addr D8WLQ472BePIAn1nRoJ6MH_GLgmlogOCVmCbp2UYSoE
