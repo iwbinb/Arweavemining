b8b07f6a187d18d94cd54c7a8fb75b7332c6f268
iwbinb <34509975+iwbinb@users.noreply.github.com>
2018-08-01 16:01:55 +0800

    Update try.sh

--- try.sh
+++ try.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "-----START123----"
-./arweave-server &&
-peer 35.178.129.36 &&
+echo "-----START1----"
+./arweave-server && \
+peer 35.178.129.36 && \
 mine mining_addr D8WLQ472BePIAn1nRoJ6MH_GLgmlogOCVmCbp2UYSoE
